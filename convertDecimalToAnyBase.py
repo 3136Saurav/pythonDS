92e318c2ee4fdb96d1e8c23bb2c93b16be975992
Saurav Rathore <40024026+3136Saurav@users.noreply.github.com>
2019-09-12 09:17:58 +0530

    Update convertDecimalToAnyBase.py

--- convertDecimalToAnyBase.py
+++ convertDecimalToAnyBase.py
@@ -2,6 +2,8 @@ from Stacks_Python import Stack
 
 def convert_decimal_to_any_base(n, b):
     remStack = Stack()
+    digits = '0123456789ABCDEF'
+
     while n>0:
         rem = n%b
         remStack.push(rem)
@@ -9,12 +11,12 @@ def convert_decimal_to_any_base(n, b):
 
     result = ''
     while not remStack.isEmpty():
-        result += str(remStack.pop())
+        result += (digits[remStack.pop()])
 
     return result
 
 if __name__ == '__main__':
     n = int(input('Enter any number: '))
     b = int(input('Enter any base: '))
-    result = convert_to_any_base(n,b)
+    result = convert_decimal_to_any_base(n,b)
     print('Result --> ',result)
